7550abf0e4ed7c0cb6ab3f089481735229a91c43
Christina Zektser <czektser1@baylor.edu>
2023-02-18 14:55:16 -0600

    Data model with JPA

--- src/main/java/com/example/bearable/authentication/model/Department.java
+++ src/main/java/com/example/bearable/authentication/model/Department.java
@@ -1,10 +1,55 @@
 package com.example.bearable.authentication.model;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
 
-@Data
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
 public class Department {
-    private final Long id;
-    private final String Name;
-    private final Boolean isEnabled;
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private String description;
+    private Boolean isEnabled;
+
+    @ManyToMany
+    @JoinTable(name = "ACCOUNT_DEPARTMENT",
+            joinColumns = {@JoinColumn(name="DEPARTMENT_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "ACCOUNT_ID", referencedColumnName = "ID")})
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    private Set<Account> accounts = new HashSet<>();
+
+    public UUID getId(){
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name  = name;
+    }
+
+    public String  getName(){
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String  getDescription(){
+        return description;
+    }
+
+    public void setIsEnabled(boolean isEnabled){
+        this.isEnabled = isEnabled;
+    }
+
 }
